518c9fb5b196437a52b8466c4f5ad1555a6dbd25
jbf <jfarmeriv@gmail.com>
2023-01-26 04:24:57 -0500

    Testing

--- vars/default_ms_pipeline.groovy
+++ vars/default_ms_pipeline.groovy
@@ -7,17 +7,15 @@ void call(Closure body) {
     body.resolveStrategy = Closure.DELEGATE_FIRST
 
     echo 'before delegate to string start'
-    echo this.toString()
+    echo this.print(this)
     echo'before delegate to string start ending '
     body.delegate = this; body()
 
   echo 'after delegate to string start'
-  echo this.toString()
-
+   this.print(this)
 
   body.delegate = env; body()
 
-    echo this.toString()
 
     node {
         sh 'env'
